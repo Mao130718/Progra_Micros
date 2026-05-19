aee627940fde938810e61c000a13c15fe2b6d0b8
Mao130718 <joa24827@uvg.edu.gt>
2026-05-19 01:19:03 -0600

    Create UART.h

---
+++ PROYECTO2_OJOS/PROYECTO2_OJOS/UART.h
@@ -0,0 +1,24 @@
+﻿/*
+ * UART.h
+ *
+ * Created: 11/05/2026 00:30:34
+ *  Author: María Olga
+ */ 
+
+#ifndef UART_H
+#define UART_H
+
+#include "AVR_CORE.h"
+#include "PWM.h"
+#include "EEPROM.h"
+
+// Inicializa el buffer de comandos y muestra el menu
+void uart_cmd_init(void);
+
+// Procesa los bytes disponibles en el buffer 
+void uart_cmd_procesar(void);
+
+// Se ejecuta la secuencia de parpadeo de ojos
+void uart_cmd_parpadeo(void);
+
+#endif /* UART_H */
